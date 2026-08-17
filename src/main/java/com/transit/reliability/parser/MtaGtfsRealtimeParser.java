e196bd37646bf4efec9afe8bb372c3a3907457c4
adityahemdev <adityaph211@gmail.com>
2026-08-17 04:35:36 -0400

    added lombok dependency, logs for missing timestamps, trip id

--- src/main/java/com/transit/reliability/parser/MtaGtfsRealtimeParser.java
+++ src/main/java/com/transit/reliability/parser/MtaGtfsRealtimeParser.java
@@ -3,11 +3,13 @@ package com.transit.reliability.parser;
 import com.google.transit.realtime.GtfsRealtime.FeedMessage;
 import com.google.transit.realtime.GtfsRealtime.FeedEntity;
 import com.google.transit.realtime.GtfsRealtime.TripUpdate;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Optional;
 
+@Slf4j
 public class MtaGtfsRealtimeParser {
 
     /**
@@ -23,25 +25,19 @@ public class MtaGtfsRealtimeParser {
         }
         FeedMessage feedMessage = FeedMessage.parseFrom(inputStream);
         
-        if (feedMessage.getHeader().hasTimestamp()) {
-            System.out.println("FeedHeader timestamp: " + feedMessage.getHeader().getTimestamp());
-        } else {
-            System.out.println("FeedHeader timestamp: not present");
+        if (!feedMessage.getHeader().hasTimestamp()) {
+            log.info("FeedHeader timestamp: not present");
         }
 
         for (FeedEntity entity : feedMessage.getEntityList()) {
             if (entity.hasTripUpdate()) {
                 TripUpdate tripUpdate = entity.getTripUpdate();
-                if (tripUpdate.hasTimestamp()) {
-                    System.out.println("TripUpdate timestamp: " + tripUpdate.getTimestamp());
-                } else {
-                    System.out.println("TripUpdate timestamp: not present");
+                if (!tripUpdate.hasTimestamp()) {
+                    log.info("TripUpdate timestamp: not present");
                 }
                 
-                if (tripUpdate.getTrip().hasTripId()) {
-                    System.out.println("TripId: " + tripUpdate.getTrip().getTripId());
-                } else {
-                    System.out.println("TripId: not present");
+                if (!tripUpdate.getTrip().hasTripId()) {
+                    log.warn("TripId: not present");
                 }
                 
                 return Optional.of(tripUpdate);
